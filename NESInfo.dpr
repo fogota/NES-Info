bd6ce426053385e3c147aa11130a3bd619fe01b2
fogota <413640314@qq.com>
2014-06-13 23:25:51 +0800

    稳定版
    Signed-off-by: fogota <413640314@qq.com>

---
+++ NESInfo.dpr
@@ -0,0 +1,14 @@
+program NESInfo;
+
+uses
+  Forms,
+  Unit1 in 'Unit1.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.Title := 'FC/NES ROM пео╒';
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
